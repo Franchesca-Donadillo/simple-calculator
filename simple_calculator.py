ca729a3cdc1dcaf006f218946222755160186e79
Franchesca Marie Donadillo <franchescamariedonadillo@gmail.com>
2023-05-06 23:57:29 +0800

    user input

--- simple_calculator.py
+++ simple_calculator.py
@@ -3,7 +3,12 @@
 # Assignment 4 - Simple Calculator
 
 # ask user to choose between four math oparations
-# Ask user for two numbers
+try:
+    user_operation = input("Enter what math operation you want to use: ").upper()
+    # Ask user for two numbers
+    num_1 = int(input("Enter first number: "))
+    num_2 = int(input("Enter second number: "))
+
 # perform calculations
 # use exeption to capture errors
 # display result if there are no errors
