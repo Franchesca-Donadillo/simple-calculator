01c60278058e24132c128774e30e7521437fa77b
Franchesca Marie Donadillo <franchescamariedonadillo@gmail.com>
2023-05-06 23:47:19 +0800

    pseudocode

---
+++ simple_calculator.py
@@ -0,0 +1,13 @@
+# Franchesca Marie U. Donadillo
+# BSCPE 1-5
+# Assignment 4 - Simple Calculator
+
+# ask user to choose between four math oparations
+# Ask user for two numbers
+# perform calculations
+# use exeption to capture errors
+# display result if there are no errors
+# ask user if they want to repeat
+# use while loop
+# if yes loop from the start
+# if no, exit the program 
